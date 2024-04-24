8ceeeaaec77cd3c46e2e00be3328aedf1f37c3ce
APakhomov <APakhomov@inno.tech>
2024-04-24 13:17:54 +0700

    Task3

---
+++ src/ru/сourses/task3/math/Fractionable.java
@@ -0,0 +1,8 @@
+package ru.сourses.task3.math;
+
+public interface Fractionable{
+    double doubleValue();
+    void setNum(int num);
+    void setDenum(int denum);
+    void setCounter(int counter);
+}
